ed17f040c81b900e05da31921cec2a00268c4098
Benjamin Rutledge <benjaminrutledge@Benjamins-MacBook-Pro-2.local>
2019-09-24 15:36:36 +0100

    First commit

---
+++ generator.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+const { exec } = require('child_process');
+const readLine = require('readline');
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function generateProject(done) {
+  fs.writeFile('index.js', '', (err, indexFile) => {
+    if (err) console.log(err);
+    else {
+      fs.mkdir('spec', (err, specDir) => {
+        if (err) console.log(err);
+        else {
+          fs.writeFile(
+            'spec/index.spec.js',
+            "const { expect } = require('chai');\n\ndescribe('', () => {\nit('', () => {});\n});",
+            (err, specFile) => {
+              if (err) console.log(err);
+              else {
+                exec('npm init -y\nnpm i mocha chai -D', (err, commands) => {
+                  if (err) console.log(err);
+                  else {
+                    fs.readFile('package.json', (err, readPackage) => {
+                      if (err) console.log(err);
+                      else {
+                        const parsed = JSON.parse(readPackage);
+                        parsed.scripts.test = 'mocha spec';
+                        const stringified = JSON.stringify(parsed);
+                        fs.writeFile(
+                          'package.json',
+                          stringified,
+                          (err, response) => {
+                            if (err) console.log(err);
+                            else {
+                              fs.writeFile(
+                                '.gitignore',
+                                'node_modules',
+                                (err, response) => {
+                                  if (err) console.log(err);
+                                  else {
+                                    fs.writeFile(
+                                      'README.md',
+                                      'Insert contents of README file',
+                                      (err, response) => {
+                                        if (err) console.log(err);
+                                        else {
+                                          rl.question(
+                                            'Would you like to initialize a github repo? (y/n)',
+                                            answer => {
+                                              if (answer === 'n') done(null);
+                                              if (answer === 'y') {
+                                                exec(
+                                                  'git init\ngit add .\ngit commit -m "First commit"',
+                                                  (err, response) => {
+                                                    if (err) console.log(err);
+                                                    else {
+                                                      rl.question(
+                                                        "Paste your github repository's quick setup link...",
+                                                        answer => {
+                                                          exec(
+                                                            `git remote add origin ${answer}\ngit remote -v\ngit push -u origin master`,
+                                                            (err, response) => {
+                                                              if (err)
+                                                                console.log(
+                                                                  err
+                                                                );
+                                                              else done(null);
+                                                            }
+                                                          );
+                                                        }
+                                                      );
+                                                    }
+                                                  }
+                                                );
+                                              }
+                                            }
+                                          );
+                                        }
+                                      }
+                                    );
+                                  }
+                                }
+                              );
+                            }
+                          }
+                        );
+                      }
+                    });
+                  }
+                });
+              }
+            }
+          );
+        }
+      });
+    }
+  });
+}
+
+generateProject((err, response) => {
+  if (err) console.log(err);
+  else {
+    console.log('Project Generated');
+  }
+});
